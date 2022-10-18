3720a3bd838ee9554c1074e03fec0a1e8e771ec1
AYJ <ayj123@1utar,my>
2022-10-18 09:51:55 +0800

    learn routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { BookdetailComponent } from './bookdetail/bookdetail.component';
+import { BooklistComponent } from './booklist/booklist.component';
+import { CrudComponent } from './crud/crud.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:"mainpage",pathMatch:"full"},
+
+  {path:"dashboard",component:DashboardComponent},
+  {path:"mainpage",component:MainpageComponent},
+  {path:"bookdetail",component:BookdetailComponent},
+  {path:"booklist",component:BooklistComponent},
+  {path:"crud",component:CrudComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
